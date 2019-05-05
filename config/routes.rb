57fb9348aa80471b6477b893d3a698f37a107e9c
kaushikhande <kaushikhande302@gmail.com>
2019-05-06 01:41:22 +0530

    navbar-addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'signup', to: 'users#new'
   root 'static_pages#home'
+
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
