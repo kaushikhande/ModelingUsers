097431ce8b6534a6933e3489e17b3f8f682d624e
kaushikhande <kaushikhande302@gmail.com>
2019-05-18 15:33:41 +0530

    login-functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'signup', to: 'users#new'
   root 'static_pages#home'
-
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
